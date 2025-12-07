fded0098a8a2bd259bc04adc46f916b68ec1794b
Tran Phuoc Dung Dang <dangphuocdung07@gmail.com>
2025-12-07 02:47:51 -0500

    Xoa file Config.h cu va ignore file config.h moi

--- include/Config.h
+++
@@ -1,9 +0,0 @@
-//Wifi Config
-
-#define WIFI_SSID "Dang Tran Phuoc Dung"
-#define PASSWORD "12345678"
-#define API_KEY "AIzaSyCXod4ZFvS270oAhHXNGBdI1h-FLPMn8zY"
-#define DATABES_URL "https://save-temp-humid-data-default-rtdb.firebaseio.com/"
-#define MQTT_SERVER "broker.hivemq.com"
-#define MQTT_TOPIC_COMMAND "house/livingroom/led/set"
-
